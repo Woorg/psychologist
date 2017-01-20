20658e805b7fda5485e5dc2bf633af4ddb3f57d8
Igor Gorlov <gorlov35@gmail.com>
2017-01-20 15:32:48 +0300

    complete

---
+++ markup/components/slider/slider.js
@@ -0,0 +1,18 @@
+(function () {
+
+    $(function () {
+        // Slider
+
+        $('.slider__list').owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: true
+        });
+
+    });
+
+
+})();
+
+
---
+++ markup/static/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/*
+    This file can be used as entry point for webpack!
+ */
+
+
+
+
